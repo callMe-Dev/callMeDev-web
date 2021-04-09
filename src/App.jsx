4b7d9927ff3e791425ed48c16b091030baab433b
mrLuisFer <lolesuncrak@gmail.com>
2021-04-09 09:15:46 -0500

    fix: removi linea inutil del .gitignore

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+    <p>
+      some food
+    </p>   
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React, { StrictMode } from 'react'
+import {render} from 'react-dom'
+import './index.css'
+import App from './App'
+
+const root = document.getElementById('root')
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  root
+)
