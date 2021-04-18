4a486238ee7f922ba05daa7147b9ccc93fb09edb
Catalina Villarreal <58574759+LCV13@users.noreply.github.com>
2021-04-18 14:37:38 -0300

    Aboutcommunity (#5)
    * cambie algunos caracteres por eslint
    
    * Agregué los estilos del texto
    
    * Agregué el botón 'Nuestra historia' a AboutCommunity
    
    * Agregué la imagen del server a AboutCommunity

--- src/components/AboutCommunity/AboutCommunity.jsx
+++ src/components/AboutCommunity/AboutCommunity.jsx
@@ -1,10 +1,32 @@
 import React from "react"
-import { AboutCommunityStyled } from "./AboutCommunity.styles"
+import historyBook from "../../assets/illustrations/history-book.svg"
+import callMeDevDiscord from "../../assets/illustrations/callme-dev-discord.jpeg"
+
+import {
+  AboutCommunityStyled,
+  AboutCommunityTitle,
+  SpanTitle,
+  AboutCommunityText,
+  AboutCommunityBtn,
+  AboutCommunityIcon,
+  AboutCommunityImg
+} from "./AboutCommunity.styles"
 
 export default function AboutCommunity() {
   return (
     <AboutCommunityStyled>
-      <p>Some content</p>
+      <div>
+        <AboutCommunityTitle>Sobre la <SpanTitle>comunidad</SpanTitle> </AboutCommunityTitle><br/>
+        <AboutCommunityText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt diam dui, et condimentum lorem interdum quis. Aenean posuere pulvinar massa in scelerisque. Vivamus elementum bibendum elit,
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt diam dui, et condimentum lorem interdum quis. Aenean posuere pulvinar massa in scelerisque. Vivamus elementum bibendum elit
+        </AboutCommunityText>
+        <AboutCommunityBtn type="button">
+          <AboutCommunityIcon src={historyBook} alt="Conoce nuestra historia"/>
+          Nuestra historia
+        </AboutCommunityBtn>
+      </div>
+      <AboutCommunityImg src={callMeDevDiscord} alt="callMeDev en discord" />
     </AboutCommunityStyled>
   )
 }
--- src/components/AboutCommunity/AboutCommunity.styles.js
+++ src/components/AboutCommunity/AboutCommunity.styles.js
@@ -1,5 +1,115 @@
 import styled from "styled-components"
+import { sideLayout } from "../../styles/sideLayout"
+import { screens } from "../../styles/utils/screens"
 
 export const AboutCommunityStyled = styled.article`
   padding-top: 14rem;
+    position: relative;
+  ${sideLayout()}
+
+  @media (min-width: ${screens.tablet}) {
+    padding-top: 15rem;
+  }
+
+  @media (min-width: ${screens.ipad}) {
+    padding-top: 16rem;
+  }
+
+  @media (min-width: ${screens.desk}) {
+    padding-top: 18rem;
+  }
+
+  display: block;
+
+  @media (min-width: ${screens.ipad}) {
+    display: flex;
+    justify-content: space-evenly;
+  }
+`
+export const AboutCommunityTitle = styled.p`
+color: var(--white);
+  font-weight: 600;
+  font-size: 1.9rem;
+  margin: 2rem 0;
+
+  @media (min-width: ${screens.tablet}) {
+    text-align: center;
+    width: 350px;
+    margin: 2rem auto;
+  }
+
+  @media (min-width: ${screens.ipad}) {
+    text-align: left;
+  }
+`
+
+export const SpanTitle = styled.span`
+  color: var(--yellow);
+`
+
+export const AboutCommunityText = styled.p`
+  font-weight: 400;
+  margin-bottom: 0.5rem;
+  color: var(--white);
+
+  @media (min-width: ${screens.tablet}) {
+    text-align: center;
+    width: 350px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: ${screens.ipad}) {
+    text-align: left;
+    width: 360px;
+    margin-bottom: 1rem;
+  }
+`
+export const AboutCommunityBtn = styled.button`
+  /* width: 19rem; */
+  align-items: center;
+  background: var(--blue);
+  border-radius: var(--radius);
+  border: none;
+  color: var(--white);
+  column-gap: 1rem;
+  display: flex;
+  font-size: 1.2rem;
+  font-weight: 500;
+  justify-content: center;
+  margin: 0 auto;
+  margin-top: 1.6rem;
+  outline: none;
+  padding: 0.8rem 2rem;
+  width: auto;
+  cursor: pointer;
+  transition: var(--transition);
+
+  @media (min-width: ${screens.ipad}) {
+    margin: 0;
+    padding: 0.6rem 1.5rem;
+    font-size: 1.3rem;
+    margin-top: 1.6rem;
+  }
+
+  &:hover {
+    filter: brightness(1.1);
+  }
+`
+export const AboutCommunityIcon = styled.img`
+  height: 35px;
+  width: 35px;
+`
+
+export const AboutCommunityImg = styled.img`
+  display: block;
+  height: auto;
+  margin: 3rem auto;
+  width: 370px;
+  object-fit:cover;
+
+  @media (min-width: ${screens.ipad}) {
+    margin: 0;
+    width: 770px;
+    object-fit: cover;
+  }
 `
