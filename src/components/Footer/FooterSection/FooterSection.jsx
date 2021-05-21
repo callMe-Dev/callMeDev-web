c564641e702f5f523d968e0aa7679e49be8e4392
mrLuisFer <lolesuncrak@gmail.com>
2021-05-21 17:15:58 -0500

    fix: JSDOC añadida al FooterSection
    fix: footerPages separado en un archivo externo

--- src/components/Footer/FooterSection/FooterSection.jsx
+++ src/components/Footer/FooterSection/FooterSection.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import { nanoid } from 'nanoid'
 import { Title, List, ItemList, StyledLink } from './FooterSection.styles'
 
-export const FooterSection = ({ titleText, titleColor, items }) => (
+/**
+ *
+ * @typedef {Object} TItems
+ * @property {String} title
+ * @property {String} url
+ */
+
+/**
+ * @param {String} titleText
+ * @param {String} titleColor
+ * @param {Array<TItems>} items
+ * @returns JSX.Element
+ */
+export const FooterSection = ({ titleText = '', titleColor = '', items = [] }) => (
   <section>
     <Title color={titleColor}>{titleText}</Title>
     <List>
---
+++ src/components/Footer/footerPages.js
@@ -0,0 +1,56 @@
+export const footerPages = [
+  {
+    title: 'Home',
+    color: 'var(--red)',
+    items: [
+      {
+        title: 'Comunidad',
+        url: '#about-community',
+      },
+      {
+        title: 'Features',
+        url: '#our-features',
+      },
+      {
+        title: 'Normas',
+        url: '/',
+      },
+    ],
+  },
+  {
+    title: 'Features',
+    color: 'var(--purple)',
+    items: [
+      {
+        title: 'Comunidad',
+        url: '/',
+      },
+      {
+        title: 'Features',
+        url: '/',
+      },
+      {
+        title: 'Normas',
+        url: '/',
+      },
+    ],
+  },
+  {
+    title: 'Nosotros',
+    color: 'var(--green)',
+    items: [
+      {
+        title: 'Proyectos',
+        url: '/about-us#our-projects',
+      },
+      {
+        title: 'Creadores',
+        url: '/about-us#our-creators',
+      },
+      {
+        title: 'Moderadores',
+        url: '/about-us#our-mods',
+      },
+    ],
+  },
+]
